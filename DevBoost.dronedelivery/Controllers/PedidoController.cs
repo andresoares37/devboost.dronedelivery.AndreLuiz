4588a423c7c7d107ecca8a4d943d6d381a2a1b88
Andre Soares <andresoares37@gmail.com>
2020-08-29 14:17:07 -0300

    Atualizando as atividades

--- DevBoost.dronedelivery/Controllers/PedidoController.cs
+++ DevBoost.dronedelivery/Controllers/PedidoController.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using DevBoost.dronedelivery.Data.Contexts;
-using DevBoost.dronedelivery.Models;
-using DevBoost.dronedelivery.Data.Repositories;
 using System.Device.Location;
+using DevBoost.dronedelivery.Domain;
+using DevBoost.DroneDelivery.Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using DevBoost.dronedelivery.Domain.Enum;
 
 namespace DevBoost.dronedelivery.Controllers
 {
@@ -16,27 +16,26 @@ namespace DevBoost.dronedelivery.Controllers
     [ApiController]
     public class PedidoController : ControllerBase
     {
-        private readonly IUnitOfWork _unitOfWork;
+        //private readonly IUnitOfWork _unitOfWork;
+        private readonly IPedidoService _pedidoService;
 
-        public PedidoController(IUnitOfWork unitOfWork)
+        public PedidoController(IPedidoService pedidoService)
         {
-            _unitOfWork = unitOfWork;
+            _pedidoService = pedidoService;
         }
 
         // GET: api/Pedido
-        [HttpGet]
+        [HttpGet, Authorize(Roles = "ADMIN,USER")]
         public async Task<ActionResult<IEnumerable<Pedido>>> GetPedido()
-        {
-            atualizarStatusDrones();
-
-            return _unitOfWork.Pedidos.GetAll().ToList();
+        {            
+            return Ok(await _pedidoService.GetAll());
         }
 
         // GET: api/Pedido/5
-        [HttpGet("{id}")]
+        [HttpGet("{id}"), Authorize(Roles = "ADMIN,USER")]
         public async Task<ActionResult<Pedido>> GetPedido(Guid id)
         {
-            var pedido = _unitOfWork.Pedidos.GetById(id);
+            var pedido = await _pedidoService.GetById(id);
 
             if (pedido == null)
             {
@@ -46,9 +45,9 @@ namespace DevBoost.dronedelivery.Controllers
             return pedido;
         }
 
-        // PUT: api/Pedido/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        //// PUT: api/Pedido/5
+        //// To protect from overposting attacks, enable the specific properties you want to bind to, for
+        //// more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
         public async Task<IActionResult> PutPedido(Guid id, Pedido pedido)
         {
@@ -57,27 +56,29 @@ namespace DevBoost.dronedelivery.Controllers
                 return BadRequest();
             }
 
-            _unitOfWork.Pedidos.Update(pedido);
+            await _pedidoService.Update(pedido);
+                        
+            //_unitOfWork.Pedidos.Update(pedido);
 
-            try
-            {
-                await Task.Run(
-                () =>
-                {
-                    _unitOfWork.Save();
-                });
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!PedidoExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+            //try
+            //{
+            //    await Task.Run(
+            //    () =>
+            //    {
+            //        _unitOfWork.Save();
+            //    });
+            //}
+            //catch (DbUpdateConcurrencyException)
+            //{
+            //    if (!PedidoExists(id))
+            //    {
+            //        return NotFound();
+            //    }
+            //    else
+            //    {
+            //        throw;
+            //    }
+            //}
 
             return NoContent();
         }
@@ -85,171 +86,52 @@ namespace DevBoost.dronedelivery.Controllers
         // POST: api/Pedido
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPost]
+        [HttpPost, Authorize(Roles = "ADMIN,USER")]
         public async Task<ActionResult<Pedido>> PostPedido(Pedido pedido)
         {
-            // existe drone com capacidade maior que o peso do pedido (limite maximo 12kg)
-                      
+            if (!ModelState.IsValid)
+                return BadRequest();
 
-            var dronesDisponiveisIds = _unitOfWork.DroneItinerario.GetAll().Where(d => d.StatusDrone == EnumStatusDrone.Disponivel).Select(s => s.DroneId).ToList();
+            string motivoRejeicaoPedido = string.Empty;
+            if (!_pedidoService.IsPedidoValido(pedido, out motivoRejeicaoPedido))
+                return BadRequest("Pedido rejeitado: " + motivoRejeicaoPedido);
 
-            if (!dronesDisponiveisIds.Any())
-                return BadRequest("Rejeitado: Não há entregadores disponíveis.");
+            pedido.InformarHoraPedido(DateTime.Now);
+            pedido.InformarStatus(EnumStatusPedido.AguardandoEntregador);
 
-            Drone drone = _unitOfWork.Drones.GetAll().Where(d => dronesDisponiveisIds.Contains(d.Id) && d.Capacidade >= pedido.Peso).FirstOrDefault();
-
-            if (drone == null)
-                return BadRequest("Rejeitado: Pedido acima do peso máximo aceito.");
-            
-            // calcular distancia do trajeto
-            // calcular tempo total (ida e volta) do trajeto (limite maximo 35m)
-            // existe um drone que atende essas condicoes
-
-            double distancia = CalcularDistanciaEmKilometros((double)pedido.Latitude, (double)pedido.Longitude);
-            distancia = distancia * 2;
-
-            // tempo = distancia / velocidade
-            // 80km / 40km/h = 2h
-            double tempoTrajetoCompleto = distancia / drone.Velocidade;
-            tempoTrajetoCompleto = tempoTrajetoCompleto * 60;
-
-            if (tempoTrajetoCompleto > drone.Autonomia)
-                return BadRequest("Rejeitado: Fora da área de entrega.");
-
-            //todo: Tempo médio de carga de bateria de TODOS os Drone: 1 hora
-
-            pedido.DataHora = DateTime.Now;
-            pedido.PrevisaoEntrega = DateTime.Now.AddMinutes(Convert.ToInt32(tempoTrajetoCompleto / 2));
-            pedido.Drone = drone;
-            pedido.Status = EnumStatusPedido.EmTransito;
-
-            DroneItinerario droneItinerario = _unitOfWork.DroneItinerario.GetAll().Where(i => i.DroneId == drone.Id).FirstOrDefault();
-
-            if (droneItinerario == null)
-            {
-                droneItinerario = new DroneItinerario();
-
-                droneItinerario.DataHora = DateTime.Now;
-                droneItinerario.Drone = drone;
-                droneItinerario.StatusDrone = EnumStatusDrone.EmTransito;
-
-                _unitOfWork.DroneItinerario.Insert(droneItinerario);
-            }
-            else
-            {
-                droneItinerario.DataHora = DateTime.Now;
-                droneItinerario.Drone = drone;
-                droneItinerario.StatusDrone = EnumStatusDrone.EmTransito;
-
-                _unitOfWork.DroneItinerario.Update(droneItinerario);
-            }
-
-            drone.AutonomiaRestante = drone.AutonomiaRestante - Convert.ToInt32(tempoTrajetoCompleto);
-
-            _unitOfWork.Pedidos.Insert(pedido);
-            _unitOfWork.Drones.Update(drone);
-            
-            await Task.Run(
-            () =>
-            {
-                _unitOfWork.Save();
-            });
-
+            await _pedidoService.Insert(pedido);
+                        
             return CreatedAtAction("GetPedido", new { id = pedido.Id }, pedido);
-
-
         }
 
         // DELETE: api/Pedido/5
-        [HttpDelete("{id}")]
+        [HttpDelete("{id}"), Authorize(Roles = "ADMIN,USER")]
         public async Task<ActionResult<Pedido>> DeletePedido(Guid id)
         {
 
+            var pedido = await _pedidoService.GetById(id);
 
-            var pedido = _unitOfWork.Pedidos.GetById(id);
             if (pedido == null)
             {
                 return NotFound();
             }
 
-            _unitOfWork.Pedidos.Delete(pedido);
-            
-            await Task.Run(
-            () =>
-            {
-                _unitOfWork.Save();
-            });
+            var result = await _pedidoService.Delete(pedido);
 
-            return pedido;
+            if (!result)
+                return BadRequest();
+
+            return Ok(pedido);
         }
 
         private bool PedidoExists(Guid id)
         {
-            var pedido = _unitOfWork.Pedidos.GetById(id);
+            //var pedido = _unitOfWork.Pedidos.GetById(id);
 
-            if (pedido == null)
-                return false;
+            //if (pedido == null)
+            //    return false;
 
             return true;
-        }
-
-
-
-        private double CalcularDistanciaEmKilometros(double latitudeDestino, double longitudeDestino)
-        {
-            var origemCoord = new GeoCoordinate(-23.5880684, -46.6564195); //local delivery
-            var destinoCoord = new GeoCoordinate(latitudeDestino, longitudeDestino);
-
-            var distance = origemCoord.GetDistanceTo(destinoCoord);
-
-            distance = distance / 1000;
-
-            return distance;
-        }
-
-        private void atualizarStatusDrones()
-        {
-            // lista itinerario nao disponíveis
-
-            var droneItinerarios = _unitOfWork.DroneItinerario.GetAll().Where(d => d.StatusDrone != EnumStatusDrone.Disponivel).ToList();
-
-            foreach (var droneItinerario in droneItinerarios)
-            {
-                droneItinerario.Drone = _unitOfWork.Drones.GetById(droneItinerario.DroneId);
-
-                if (droneItinerario.StatusDrone == EnumStatusDrone.Carregando)
-                {
-                    if (DateTime.Now.Subtract(droneItinerario.DataHora).Minutes >= 60)
-                    {
-                        droneItinerario.StatusDrone = EnumStatusDrone.Disponivel;
-                        droneItinerario.Drone.AutonomiaRestante = droneItinerario.Drone.Autonomia;
-                        droneItinerario.DataHora = DateTime.Now;
-                    }
-                }
-                else if (droneItinerario.StatusDrone == EnumStatusDrone.EmTransito)
-                {
-                    var pedido = _unitOfWork.Pedidos.GetAll().Where(p => p.DroneId == droneItinerario.DroneId && p.Status == EnumStatusPedido.EmTransito).FirstOrDefault();
-
-                    int tempoEntrega = pedido.PrevisaoEntrega.Subtract(pedido.DataHora).Minutes;
-
-                    if (pedido.PrevisaoEntrega.AddMinutes(tempoEntrega) <= DateTime.Now)
-                    {
-                        if (droneItinerario.Drone.AutonomiaRestante <= 5)
-                            droneItinerario.StatusDrone = EnumStatusDrone.Carregando;
-                        else
-                            droneItinerario.StatusDrone = EnumStatusDrone.Disponivel;
-
-                        droneItinerario.DataHora = DateTime.Now;
-                        pedido.Status = EnumStatusPedido.Entregue;
-
-                        _unitOfWork.Pedidos.Update(pedido);
-                    }
-                }
-
-                _unitOfWork.DroneItinerario.Update(droneItinerario);
-            }
-
-            _unitOfWork.Save();
         }
     }
 }
--- DevBoost.dronedelivery/Startup.cs
+++ DevBoost.dronedelivery/Startup.cs
@@ -1,16 +1,20 @@
+using DevBoost.DroneDelivery.Application.Services;
+using DevBoost.DroneDelivery.Domain.Interfaces.Repositories;
+using DevBoost.DroneDelivery.Domain.Interfaces.Services;
+using DevBoost.DroneDelivery.Repository;
+using DevBoost.DroneDelivery.Repository.Context;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using DevBoost.dronedelivery.Data.Contexts;
-using DevBoost.dronedelivery.Data.Repositories;
+using System.Text;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using DevBoost.DroneDelivery.Application.Resources;
 
 namespace DevBoost.dronedelivery
 {
@@ -27,9 +31,59 @@ namespace DevBoost.dronedelivery
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddDbContext<PedidoContext>();
-            services.AddTransient<IUnitOfWork, UnitOfWork>();
-            services.AddTransient(typeof(IRepository<>), typeof(EFRepository<>));
+            //services.AddDbContext<PedidoContext>();
+            //services.AddTransient<IUnitOfWork, UnitOfWork>();
+            //services.AddTransient<IDeliveryService, DeliveryService>();
+            //services.AddTransient(typeof(IRepository<>), typeof(EFRepository<>));
+            //services.AddTransient<IUserService, UserService>();
+
+            services.AddSwaggerGen(c => c.SwaggerDoc(name: "v1", new OpenApiInfo
+            {
+                Title = "Drone Delivery",
+                Version = "v1",
+            }));
+
+            var key = Encoding.ASCII.GetBytes(SecretToken.Key);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ClockSkew = TimeSpan.FromMinutes(30),
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+            //services.AddDbContext<PedidoContext>();
+            //services.AddTransient<IUnitOfWork, UnitOfWork>();
+            //services.AddTransient(typeof(IRepository<>), typeof(EFRepository<>));
+
+            services.AddTransient<DCDroneDelivery>();
+            services.AddScoped<IPedidoService, PedidoService>();
+            services.AddScoped<IDroneService, DroneService>();
+            services.AddScoped<IDroneItinerarioService, DroneItinerarioService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IDroneItinerarioRepository, DroneItinerarioRepository>();
+            services.AddScoped<IDroneRepository, DroneRepository>();
+            services.AddScoped<IPedidoRepository, PedidoRepository>();            
+
+
+            services.AddDbContext<DCDroneDelivery>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+                    m => m.MigrationsAssembly("DroneDelivery"));
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,9 +94,20 @@ namespace DevBoost.dronedelivery
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+
             app.UseRouting();
 
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            app.UseAuthentication();
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "Drone Delivery"); });
 
             app.UseEndpoints(endpoints =>
             {
